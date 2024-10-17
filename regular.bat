38a2d38bf89ae01a368c6d0bd8117143d27fdfea
radeeven <radeeven@gmail.com>
2024-10-17 12:16:12 +0200

    Lowered max cpu, added changing of GPU Profiles with MSI Afterburner
    I have a new more beefy computer, which requires more throttling. I also started using WP Local, which actually uses a bit of GPU, requiring GPU Throttling as well.

--- regular.bat
+++ regular.bat
@@ -5,3 +5,4 @@ powercfg -setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX 100
 powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR SYSCOOLPOL 1
 powercfg -setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR SYSCOOLPOL 1
 powercfg.exe -setactive SCHEME_CURRENT
+start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -profile1
--- stfu.bat
+++ stfu.bat
@@ -1,7 +1,8 @@
 powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMIN 0
 powercfg -setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMIN 0
-powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX 20
-powercfg -setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX 20
+powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX 7
+powercfg -setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX 7
 powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR SYSCOOLPOL 0
 powercfg -setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR SYSCOOLPOL 0
 powercfg.exe -setactive SCHEME_CURRENT
+start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -profile2
